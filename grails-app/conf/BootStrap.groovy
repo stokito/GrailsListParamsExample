396af94ae677a37f162db7266a6b1eca3d63e8d9
Sergey Ponomarev <stokito@gmail.com>
2013-09-26 00:15:26 +0300

    BootStrap.groovy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,11 +3,16 @@ import name.stokito.listParamsExample.User
 class BootStrap {
 
     def init = { servletContext ->
-        30.times {
-            User user = new User(email: "user${it}@example.com", name: "User${it}")
-            user.save(flush: true, failOnError: true)
+        environments {
+            development {
+                30.times {
+                    User user = new User(email: "user${it}@example.com", name: "User${it}")
+                    user.save(flush: true, failOnError: true)
+                }
+            }
         }
     }
+
     def destroy = {
     }
 }
