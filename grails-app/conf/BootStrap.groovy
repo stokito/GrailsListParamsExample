2ee3450fb90c6f82ad04e9faf95953310e70c96b
Sergey Ponomarev <stokito@gmail.com>
2013-09-25 23:04:18 +0300

    30 users

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,7 +3,7 @@ import name.stokito.listParamsExample.User
 class BootStrap {
 
     def init = { servletContext ->
-        10.times {
+        30.times {
             User user = new User(email: "user${it}@example.com", name: "User${it}")
             user.save(flush: true, failOnError: true)
         }
