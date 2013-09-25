c80d1382ade1726d9eaed0e033171e21b270dc3b
Sergey Ponomarev <stokito@gmail.com>
2013-09-25 23:38:45 +0300

    UserControllerSpec.groovy

--- test/unit/name/stokito/listParamsExample/UserControllerSpec.groovy
+++ test/unit/name/stokito/listParamsExample/UserControllerSpec.groovy
@@ -11,8 +11,8 @@ class UserControllerSpec extends Specification {
 
     def populateValidParams(params) {
         assert params != null
-        // TODO: Populate valid properties like...
-        //params["name"] = 'someValidName'
+        params["email"] = 'user@example.com'
+        params["name"] = 'someValidName'
     }
 
     void "Test the index action returns the correct model"() {
